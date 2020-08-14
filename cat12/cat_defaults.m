9dc2605059d59421ec70d579f7328333f5fe765f
Kelvin Sarink <k.sarink@uni-muenster.de>
2020-08-14 14:56:26 +0200

    update to spm12-v7771 cat-v1653 and add smoothing 10 and 12

--- cat12/cat_defaults.m
+++ cat12/cat_defaults.m
@@ -7,9 +7,9 @@ function cat_defaults
 %
 % Care must be taken when modifying this file
 %_______________________________________________________________________
-% $Id: cat_defaults.m 1436 2019-03-06 16:46:15Z gaser $
+% $Id: cat_defaults.m 1618 2020-05-13 12:58:08Z gaser $
 
-if exist('cat','var'), clear cat; end 
+clear global cat; 
 global cat
 
 
@@ -30,9 +30,9 @@ cat.opts.biasreg   = 0.001;                   % Bias regularisation (cat.opts.bi
 cat.opts.biasfwhm  = 60;                      % Bias FWHM (cat.opts.biasstr has to be 0!) - 30:10:120,inf 
                                               %   lower values for strong bias fields, but check for overfitting of the thalamus (values <45 mm)
 cat.opts.samp      = 3;                       % Sampling distance - alternative: 1.5 
-                                              %   Initial SPM segmentation resolution, whereas the AMAP runs on the full or speciefied resolution
+                                              %   Initial SPM segmentation resolution, whereas the AMAP runs on the full or specified resolution
                                               %   described by cat.extopts.restype and cat.extopts.resval. Higher resolution did not improve the
-                                              %   results in most results (but increase caluculation time were.  
+                                              %   results in most results (but increase calculation time were.  
 cat.opts.redspmres = 0.0;                     % limit image resolution for internal SPM preprocessing output in mm (default: 1.0)
 
                                               
@@ -101,10 +101,21 @@ cat.output.label.native = 1;
 cat.output.label.warped = 0;
 cat.output.label.dartel = 0;
 
+% Tissue classes 4-6 to create own TPMs
+cat.output.TPMC.native = 0; 
+cat.output.TPMC.warped = 0;
+cat.output.TPMC.mod    = 0;
+cat.output.TPMC.dartel = 0;
+
 % cortical thickness (experimental)
 cat.output.ct.native = 0; 
 cat.output.ct.warped = 0;
 cat.output.ct.dartel = 0;
+
+% percentage position (experimental)
+cat.output.pp.native = 0; 
+cat.output.pp.warped = 0;
+cat.output.pp.dartel = 0;
 
 % jacobian determinant 0/1 (none/yes)
 cat.output.jacobian.warped = 1;
@@ -134,18 +145,25 @@ cat.extopts.spm_kamap    = 0;    % Replace initial SPM by k-means AMAP segm.  0 
 cat.extopts.NCstr        =-Inf;  % Strength of the noise correction:          0 to 1; 0 - no filter, -Inf - auto, 1 - full, 2 - ISARNLM (else SANLM), default -Inf
 cat.extopts.LASstr       = 0.5;  % Strength of the local adaption:            0 to 1; default 0.5
 cat.extopts.BVCstr       = 0.5;  % Strength of the Blood Vessel Correction:   0 to 1; default 0.5
-cat.extopts.regstr       = 0;    % Strength of Shooting registration:         0 - Dartel, eps (fast), 0.5 (default) to 1 (accurate) optimized Shooting, 4 - default Shooting; default 0
+cat.extopts.regstr       = 0.5;  % Strength of Shooting registration:         0 - Dartel, eps (fast), 0.5 (default) to 1 (accurate) optimized Shooting, 4 - default SPM Shooting
 cat.extopts.WMHC         = 1;    % Correction of WM hyperintensities:         0 - no correction, 1 - only for Dartel/Shooting
                                  %                                            2 - also correct segmentation (to WM), 3 - handle as separate class; default 1
 cat.extopts.WMHCstr      = 0.5;  % Strength of WM hyperintensity correction:  0 to 1; default 0.5
 cat.extopts.SLC          = 0;    % Stroke lesion correction (SLC):            0 - no correction, 1 - handling of manual lesion that have to be set to zero!
                                  %                                            2 - automatic lesion detection (in development)
 cat.extopts.mrf          = 1;    % MRF weighting:                             0 to 1; <1 - weighting, 1 - auto; default 1
-cat.extopts.INV          = 1;    %  Invert PD/T2 images for preprocessing:    0 - no processing, 1 - try intensity inversion, 2 - synthesize T1 image; default 1
+cat.extopts.INV          = 1;    % Invert PD/T2 images for preprocessing:    0 - no processing, 1 - try intensity inversion, 2 - synthesize T1 image; default 1
 
 % resolution options
-cat.extopts.restype      = 'fixed';      % resolution handling: 'native','fixed','best'
+cat.extopts.restype      = 'optimal';    % resolution handling: 'native','fixed','best', 'optimal'
 cat.extopts.resval       = [1.0 0.10];   % resolution value and its tolerance range for the 'fixed' and 'best' restype
+
+% check for multiple cores is different for octave
+if strcmpi(spm_check_version,'octave')
+  cat.extopts.nproc      = nproc;
+else
+  cat.extopts.nproc      = feature('numcores');
+end
 
 %{
 native:
@@ -186,6 +204,17 @@ fixed:
       [1.00 0.02]     0.95 1.05 1.25     >     1.00 1.00 1.00
       [0.75 0.10]     0.75 0.95 1.25     >     0.75 0.75 0.75
 
+optimal: 
+    This option prefers an isotropic voxel size that is controlled by the median voxel size and a volume term that deals with highly anisotropic voxels.  
+    The first parameter controls the lower resolution limit, while the second parameter is used to avoid tiny interpolations for almost correct resolutions. 
+ 
+    Examples: 
+      Parameters     native resolution       internal resolution
+      [1.00 0.10]     0.50 0.50 0.90     >     0.50 0.50 0.60
+      [1.00 0.10]     0.50 0.50 1.00     >     0.70 0.70 0.70
+      [1.00 0.10]     0.80 0.80 1.00     >     1.00 1.00 1.00
+      [1.00 0.10]     0.45 0.45 1.70     >     0.90 0.90 0.90
+      [1.00 0.10]     0.95 1.05 1.25     >     0.95 1.05 1.00
 %}
 
 
@@ -195,33 +224,47 @@ cat.extopts.species      = 'human';
 % Affine PreProcessing (APP) with rough bias correction and brain extraction for special anatomies (nonhuman/neonates) 
 cat.extopts.APP          = 1070;  % 0 - none; 1070 - default; [1 - light; 2 - full; 1144 - update of 1070, 5 - animal (no affreg)]
 cat.extopts.vox          = 1.5;   % voxel size for normalized data (EXPERIMENTAL:  inf - use Tempate values)
-cat.extopts.darteltpm    = {fullfile(spm('dir'),'toolbox','cat12','templates_1.50mm','Template_1_IXI555_MNI152.nii')};     % Indicate first Dartel template (Template_1)
-cat.extopts.shootingtpm  = {fullfile(spm('dir'),'toolbox','cat12','templates_1.50mm','Template_0_IXI555_MNI152_GS.nii')};  % Indicate first Shooting template (Template 0) - not working
-cat.extopts.cat12atlas   = {fullfile(spm('dir'),'toolbox','cat12','templates_1.50mm','cat.nii')};                       % CAT atlas with major regions for VBM, SBM & ROIs
+cat.extopts.darteltpm    = {fullfile(spm('dir'),'toolbox','cat12','templates_volumes','Template_1_IXI555_MNI152.nii')};     % Indicate first Dartel template (Template_1)
+cat.extopts.shootingtpm  = {fullfile(spm('dir'),'toolbox','cat12','templates_volumes','Template_0_IXI555_MNI152_GS.nii')};  % Indicate first Shooting template (Template 0) - not working
+cat.extopts.cat12atlas   = {fullfile(spm('dir'),'toolbox','cat12','templates_volumes','cat.nii')};                       % CAT atlas with major regions for VBM, SBM & ROIs
 cat.extopts.brainmask    = {fullfile(spm('Dir'),'toolbox','FieldMap','brainmask.nii')};                                 % Brainmask for affine registration
 cat.extopts.T1           = {fullfile(spm('Dir'),'toolbox','FieldMap','T1.nii')};                                        % T1 for affine registration
 
 % surface options
-cat.extopts.pbtres         = 0.5;   % internal resolution for thickness estimation in mm (default 0.5) 
-cat.extopts.close_parahipp = 0;     % optionally apply closing inside mask for parahippocampal gyrus to get rid of the holes that lead to large
-                                    % cuts in gyri after topology correction. However, this may also lead to poorer quality of topology 
-                                    % correction for other data and should be only used if large cuts in the parahippocampal areas occur
-cat.extopts.scale_cortex   = 0.7;   % scale intensity values for cortex to start with initial surface that is closer to GM/WM border to prevent that gyri/sulci are glued 
-                                    % if you still have glued gyri/sulci (mainly in the occ. lobe) you can try to decrease this value (start with 0.6)
-                                    % please note that decreasing this parameter also increases the risk of an interrupted parahippocampal gyrus
-cat.extopts.add_parahipp   = 0.1;   % increase values in the parahippocampal area to prevent large cuts in the parahippocampal gyrus (initial surface in this area
-                                    % will be closer to GM/CSF border)
-                                    % if the parahippocampal gyrus is still cut you can try to increase this value (start with 0.15)
+cat.extopts.pbtres         = 0.5; % internal resolution for thickness estimation in mm (default 0.5) 
+cat.extopts.collcorr       = 0;   % correction of surface collisions (experimental, not yet working properly!): 0 - none; 1 - Deformation; 20 - none2; 21 - Deformation; 22 - CAT_SI; 23 - PBT; 24 - PBT opt. 
+cat.extopts.reduce_mesh    = 1;   % optimize surface sampling: 0 - PBT res. (slow); 1 - optimal res. (default); 2 - internal res.; 3 - SPM init; 4 - MATLAB init; 5 - SPM full; 6 - MATLAB full; 7 - MATLAB full ext.;
+cat.extopts.vdist          = 4/3; % mesh resolution (experimental, do not change!)
+cat.extopts.pbtlas         = 0;   % reduce myelination effects (experimental, not yet working properly!)
+cat.extopts.thick_measure  = 1;   % distance method for estimating thickness:  1 - Tfs: Freesurfer method using mean(Tnear1,Tnear2) (default in 12.7+); 0 - Tlink: linked distance (used before 12.7)
+cat.extopts.thick_limit    = 5;   % upper limit for Tfs thickness measure similar to Freesurfer (only valid if cat.extopts.thick_measure is set to "1"
+cat.extopts.close_parahipp = 0;   % optionally apply closing inside mask for parahippocampal gyrus to get rid of the holes that lead to large
+                                  % cuts in gyri after topology correction. However, this may also lead to poorer quality of topology 
+                                  % correction for other data and should be only used if large cuts in the parahippocampal areas occur
+cat.extopts.scale_cortex   = 0.7; % scale intensity values for cortex to start with initial surface that is closer to GM/WM border to prevent that gyri/sulci are glued 
+                                  % if you still have glued gyri/sulci (mainly in the occ. lobe) you can try to decrease this value (start with 0.6)
+                                  % please note that decreasing this parameter also increases the risk of an interrupted parahippocampal gyrus
+cat.extopts.add_parahipp   = 0.1; % increase values in the parahippocampal area to prevent large cuts in the parahippocampal gyrus (initial surface in this area
+                                  % will be closer to GM/CSF border)
+                                  % if the parahippocampal gyrus is still cut you can try to increase this value (start with 0.15)
 
 % visualisation, print, developing, and debugging options
 cat.extopts.colormap     = 'BCGWHw'; % {'BCGWHw','BCGWHn'} and matlab colormaps {'jet','gray','bone',...};
 cat.extopts.verb         = 2;     % verbose output:        1 - default; 2 - details; 3 - write debugging files 
-cat.extopts.ignoreErrors = 0;     % catch errors:          0 - stop with error (default); 1 - catch preprocessing errors (requires MATLAB 2008 or higher); 
+cat.extopts.ignoreErrors = 1;     % catch errors:          0 - stop with error (default); 1 - catch preprocessing errors (requires MATLAB 2008 or higher); 
 cat.extopts.expertgui    = 1;     % control of user GUI:   0 - common user modus with simple GUI; 1 - expert modus with extended GUI; 2 - developer modus with full GUI
 cat.extopts.subfolders   = 1;     % use subfolders such as mri, surf, report, and label to organize your data
-cat.extopts.experimental = 0;     % experimental functions: 0 - default, 1 - call experimental unsave functions
-cat.extopts.print        = 2;     % display and print out pdf-file of results: 0 - off, 2 - volume only, 2 - volume and surface (default)
-
+cat.extopts.experimental = 0;     % experimental functions: 0 - default, 1 - call experimental unsafe functions
+cat.extopts.print        = 2;     % display and print out pdf-file of results: 0 - off, 1 - volume only (use this to avoid problems on servers that do not support openGL), 2 - volume and surface (default)
+cat.extopts.fontsize     = get(0,'defaultuicontrolFontSize'); % default font size for GUI; 
+%cat.extopts.fontsize     = spm('FontSizes',7); % set default font size for GUI manually; increase value for larger fonts or set it to 
+cat.extopts.send_info    = 1;     % send Matlab and CAT12 version to SBM server for internal statistics only. If you don't want to send this 
+                                  % information set this flag to "0". See online help CAT12->CAT12 user statistics for more information
+cat.extopts.gifti_dat    = 1;     % save gifti files after resampling with external dat-file, which increases speed of gifti-processing and keeps SPM.mat file small
+                                  % because the cdata field is not saved with full data in SPM.mat.
+                                  
+% always use expert mode for standalone installations
+if isdeployed, cat.extopts.expertgui = 1; end
 
 % Expert options - ROIs
 %=======================================================================
@@ -232,30 +275,41 @@ cat.extopts.print        = 2;     % display and print out pdf-file of results: 0
 %  tissue      = {['csf','gm','wm','brain','none']}  - tissue classes for volume estimation
 %  use         = [ 0 | 1 ]                           - default setting to use this atlas 
 cat.extopts.atlas       = { ... 
-  fullfile(spm('dir'),'toolbox','cat12','templates_1.50mm','neuromorphometrics.nii')  1      {'csf','gm'}        1; ... % atlas based on 35 subjects
-  fullfile(spm('dir'),'toolbox','cat12','templates_1.50mm','lpba40.nii')              1      {'gm'}              1; ... % atlas based on 40 subjects
-  fullfile(spm('dir'),'toolbox','cat12','templates_1.50mm','cobra.nii')               1      {'gm','wm'}         1; ... % hippocampus-amygdala-cerebellum, 5 subjects, 0.6 mm voxel size 
-  fullfile(spm('dir'),'toolbox','cat12','templates_1.50mm','hammers.nii')             1      {'csf','gm','wm'}   1; ... % atlas based on 20 subjects
-  fullfile(spm('dir'),'toolbox','cat12','templates_1.50mm','ibsr.nii')                1      {'csf','gm'}        1; ... % less regions, 18 subjects, low T1 image quality
-  fullfile(spm('dir'),'toolbox','cat12','templates_1.50mm','aal.nii')                 1      {'gm'}              1; ... % many regions, but only labeled on one subject 
-  fullfile(spm('dir'),'toolbox','cat12','templates_1.50mm','mori.nii')                1      {'gm','wm'}         1; ... % only one subject, but with WM regions
-  fullfile(spm('dir'),'toolbox','cat12','templates_1.50mm','anatomy.nii')             1      {'gm','wm'}         1; ... % ROIs requires further work >> use Anatomy toolbox
+  fullfile(spm('dir'),'toolbox','cat12','templates_volumes','neuromorphometrics.nii')  1      {'csf','gm'}        1; ... % atlas based on 35 subjects
+  fullfile(spm('dir'),'toolbox','cat12','templates_volumes','lpba40.nii')              1      {'gm'}              1; ... % atlas based on 40 subjects
+  fullfile(spm('dir'),'toolbox','cat12','templates_volumes','cobra.nii')               1      {'gm','wm'}         1; ... % hippocampus-amygdala-cerebellum, 5 subjects, 0.6 mm voxel size 
+  fullfile(spm('dir'),'toolbox','cat12','templates_volumes','hammers.nii')             1      {'csf','gm','wm'}   1; ... % atlas based on 20 subjects
+  fullfile(spm('dir'),'toolbox','cat12','templates_volumes','ibsr.nii')                1      {'csf','gm'}        1; ... % less regions, 18 subjects, low T1 image quality
+  fullfile(spm('dir'),'toolbox','cat12','templates_volumes','aal3.nii')                1      {'gm'}              1; ... % many regions, but only labeled on one subject 
+  fullfile(spm('dir'),'toolbox','cat12','templates_volumes','mori.nii')                1      {'gm','wm'}         1; ... % only one subject, but with WM regions
+  fullfile(spm('dir'),'toolbox','cat12','templates_volumes','anatomy.nii')             1      {'gm','wm'}         1; ... % ROIs requires further work >> use Anatomy toolbox
+  fullfile(spm('dir'),'toolbox','cat12','templates_volumes','Schaefer2018_100Parcels_17Networks_order.nii') 1 {'gm'} 1; ... % atlas based on rsfMRI data from 1489 subjects
+  fullfile(spm('dir'),'toolbox','cat12','templates_volumes','Schaefer2018_200Parcels_17Networks_order.nii') 1 {'gm'} 1; ... % atlas based on rsfMRI data from 1489 subjects
+  fullfile(spm('dir'),'toolbox','cat12','templates_volumes','Schaefer2018_400Parcels_17Networks_order.nii') 1 {'gm'} 1; ... % atlas based on rsfMRI data from 1489 subjects
+  fullfile(spm('dir'),'toolbox','cat12','templates_volumes','Schaefer2018_600Parcels_17Networks_order.nii') 1 {'gm'} 1; ... % atlas based on rsfMRI data from 1489 subjects
+}; 
+
+% { name fileid GUIlevel use } - in development
+cat.extopts.satlas      = { ... 
+  'Desikan'                fullfile(spm('dir'),'toolbox','cat12','atlases_surfaces','lh.aparc_a2009s.freesurfer.annot')                        1   1;  
+  'Destrieux'              fullfile(spm('dir'),'toolbox','cat12','atlases_surfaces','lh.aparc_DK40.freesurfer.annot')                          1   1;  
+  'HCP'                    fullfile(spm('dir'),'toolbox','cat12','atlases_surfaces','lh.aparc_HCP_MMP1.freesurfer.annot')                      1   1; 
+  ... Schaefer atlases ...
+  'Schaefer2018_100P_17N'  fullfile(spm('dir'),'toolbox','cat12','atlases_surfaces','lh.Schaefer2018_100Parcels_17Networks_order.annot')       1   1; 
+  'Schaefer2018_200P_17N'  fullfile(spm('dir'),'toolbox','cat12','atlases_surfaces','lh.Schaefer2018_200Parcels_17Networks_order.annot')       1   1;
+  'Schaefer2018_400P_17N'  fullfile(spm('dir'),'toolbox','cat12','atlases_surfaces','lh.Schaefer2018_400Parcels_17Networks_order.annot')       1   1; 
+  'Schaefer2018_600P_17N'  fullfile(spm('dir'),'toolbox','cat12','atlases_surfaces','lh.Schaefer2018_600Parcels_17Networks_order.annot')       1   1; 
 }; 
 
 
+
 %=======================================================================
-% PRIVATE PARAMETER (NOT FOR GENERAL USE)
+% PRIVATE PARAMETERS (NOT FOR GENERAL USE)
 %=======================================================================
 
 
 % Additional maps
 %=======================================================================
-% Tissue classes 4-6 to create own TPMs
-cat.output.TPMC.native = 0; 
-cat.output.TPMC.warped = 0;
-cat.output.TPMC.mod    = 0;
-cat.output.TPMC.dartel = 0;
-
 % atlas maps (for evaluation)
 cat.output.atlas.native = 0; 
 cat.output.atlas.warped = 0; 
@@ -278,3 +332,4 @@ cat.extopts.LAB.HD = 21; % Head
 cat.extopts.LAB.HI = 23; % WM hyperintensities
 cat.extopts.LAB.PH = 25; % Gyrus parahippocampalis
 cat.extopts.LAB.LE = 27; % lesions
+ 
