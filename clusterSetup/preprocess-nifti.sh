aaf43f44d3753ad88047ebb71c1707c3ed0af769
dominik <dominik.grotegerd@wwu.de>
2021-01-25 15:02:39 +0100

    did some switches to enable longitudinal stuff

--- clusterSetup/preprocess-nifti.sh
+++ clusterSetup/preprocess-nifti.sh
@@ -2,7 +2,24 @@
 nifti=`realpath $1`
 id=`basename $nifti | sed 's|.nii||g'`
 timestamp=`date +'%Y-%m-%d_%H-%M-%S'`
-workingdir="/scratch/tmp/e0trap/data/MuensterNeuroimagingCohort/cat12/cjp_v0008-spm12_v7771-cat12_r1720/workingdir/${id}_$timestamp"
+workingdir="/scratch/tmp/d_grot03/for2107_long/workdir/${id}_$timestamp"
+spmdir="/home/d/d_grot03/applications/standalone_spm12_v7771_cat12_v1720-long/"
+spmolddir="/spm-data/Scratch/spielwiese_dominik/spm_mit_cat12_1720/spm12/"
+mcrdir="/home/d/d_grot03/applications/mcr/v96"
+
+# if more than 1 image, it's longitudinal
+if [ "$#" -gt 1 ]; 
+	then modality=adjust_input_long
+	theJob=cat12_complete_joint_pipeline_longitudinal.mat
+	echo Using longitudinal pipeline....
+# otherwise it's Kelvins stuff
+else
+    modality=adjust_input
+    theJob=cat12_complete_joint_pipeline.mat
+    echo Using regular pipeline....
+fi
+
+
 mkdir -p $workingdir
 cd $workingdir
 
@@ -11,15 +28,15 @@ export MCR_INHIBIT_CTF_LOCK=1
 
 batchfilename=$workingdir/${id}-spm12batch.mat
 #Create SPM12 batch 
-/scratch/tmp/e0trap/software/cat12_joint_pipeline/ksarink-cjp_v0008-spm12_v7771-cat12_r1720/run_spm12.sh \
-/scratch/tmp/e0trap/software/mcr/v96 \
-adjust_input \
-/scratch/tmp/e0trap/software/cat12_joint_pipeline/cat12_complete_joint_pipeline.mat \
+${spmdir}/run_spm12.sh \
+${mcrdir} \
+${modality} \
+${spmdir}/${theJob} \
 $batchfilename \
-$nifti \
-/spm-data/Scratch/spielwiese_kelvin/cat12_joint_pipeline/cjp_v0008-spm12_v7771-cat12_r1720-modifiedcatdefaults/ &> ./adjusting.log;
+$(realpath $@) \
+${spmolddir} &> ./adjusting.log;
 
 #Execute SPM12 batch
-/scratch/tmp/e0trap/software/cat12_joint_pipeline/ksarink-cjp_v0008-spm12_v7771-cat12_r1720/run_spm12.sh \
-/scratch/tmp/e0trap/software/mcr/v96 \
+${spmdir}/run_spm12.sh \
+${mcrdir} \
 batch $batchfilename &> ./spm12.log
