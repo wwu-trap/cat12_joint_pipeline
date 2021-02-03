f359658a9d1c8c0ce20069fd3c98672f23689c40
dominik <dominik.grotegerd@wwu.de>
2021-02-03 14:43:34 +0100

    added scripts to generate and compile the pipelines (including gitignore to ignoren directory where stuff is generated)

---
+++ make_cjp9-long.m
@@ -0,0 +1,16 @@
+function make_cjp9-long(baseFolder)
+
+if ~strcmp(version('-release'),'2017b')
+    warning('You should provide Matlab R2017b! Not Safe here, bye.')
+    exit(1);
+end
+
+restoredefaultpath
+addpath(baseFolder)
+spm('fmri');
+close all
+cd([baseFolder filesep 'config']);
+disp('Running make standalone. This will take a while, grap a coffee!');
+spm_make_standalone;
+quit(0);
+
