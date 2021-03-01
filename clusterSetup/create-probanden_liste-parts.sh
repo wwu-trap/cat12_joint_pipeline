201033c522bc511ea0fdcf47e3f4f5a9ae326859
dominik <dominik.grotegerd@wwu.de>
2021-03-01 11:20:28 +0100

    fixed some minor bug in case wildcards do not match any files

--- clusterSetup/create-probanden_liste-parts.sh
+++ clusterSetup/create-probanden_liste-parts.sh
@@ -6,7 +6,7 @@ splitter=4
 mkdir -p $workdir
 rm $workdir/* 2> /dev/null
 
-realpath ./subjects/*.nii > probanden_liste.txt
-realpath ./subjects/*.img >> probanden_liste.txt
+realpath -e -q ./subjects/*.nii > probanden_liste.txt
+realpath -e -q ./subjects/*.img >> probanden_liste.txt
 cd $workdir
 split --lines=$splitter ../probanden_liste.txt
