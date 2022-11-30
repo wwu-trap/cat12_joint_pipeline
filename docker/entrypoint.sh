f33c04bbae3700f49f7db1e70e6c54974fc2b35b
Kelvin Sarink <k.sarink@uni-muenster.de>
2022-11-30 14:44:14 +0100

    add check for mcr v96

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -42,7 +42,6 @@ function elog() {
 
 
 # Checks:
-# 1: v96 mounted? -> right version?
 # 2: /in mounted ro?
 # 3: /out mounted and writable
 # 4: $1 is .nii{,.gz} file? (gunzip when .nii.gz after copying)
@@ -57,8 +56,15 @@ BATCH_TEMPLATE="/opt/cjp8-batch-template.mat"
 BATCH_TEMPLATE_REPLACE_PATH="/spm-data/Scratch/spielwiese_kelvin/cat12_joint_pipeline/cjp_v0008-spm12_v7771-cat12_r1720-modifiedcatdefaults/"
 
 
-echo "--- Step 1: Prepare ---"
-[ -z "$1" ] && exit 1
+enotify "### Step 1: Prepare ###"
+
+if [[ ! $(grep -c "<version>9.6" /opt/mcr/v96/VersionInfo.xml 2> /dev/null) -gt 0 ]]; then
+    eerror "Wrong MCR Version - Please download, install and mount MATLAB MCR v96 to /opt/mcr/v96/"
+    exit 11
+else
+    edebug "Found MATLAB MCR v96"
+fi
+
 nifti=$(realpath "$1")
 id=$(basename "$nifti" | sed 's|.nii||g')
 timestamp=$(date +'%Y-%m-%d_%H-%M-%S')
