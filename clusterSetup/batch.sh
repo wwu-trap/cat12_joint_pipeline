a7861db4478c78badea895fd8fa1c61d1625bfb9
dominik <dominik.grotegerd@wwu.de>
2023-10-24 17:21:16 +0200

    version changed in example file

--- clusterSetup/batch.sh
+++ clusterSetup/batch.sh
@@ -8,7 +8,7 @@
 #SBATCH --partition=normal          # on which partition to submit the job
 #SBATCH --time=24:00:00             # the max wallclock time (time limit your job will run)
  
-#SBATCH --job-name=[NameForJob]-cjp_v0009-long         # the name of your job
+#SBATCH --job-name=[NameForJob]-cjp_v0010         # the name of your job
 #SBATCH --output=output-orig.dat         # the file where output is written to (stdout & stderr)
 #SBATCH --mail-type=ALL             # receive an email when your job starts, finishes normally or is aborted
 #SBATCH --mail-user=[someMail @ uni-muenster.de] # your mail address
